094ca96421e7b792fe808f9c6a9906b8c2b7aac6
Pedro Teixeira <pedro.ap.teixeira@gmail.com>
2016-12-06 19:45:39 +0000

    Modified version checking

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var path = require('path');
 var Configstore = require('configstore');
 var readPkgUp = require('read-pkg-up');
 
-function getConf(opts) {
+function getConf(opts, pkg) {
+	opts = opts || {};
+
 	var name = opts.name;
+	var version = opts.perVersion ? pkg.version : null;
 
 	if (!name) {
 		delete require.cache[__filename];
@@ -15,39 +18,40 @@ function getConf(opts) {
 		throw new Error('Couldn\'t infer the package name. Please specify it in the options.');
 	}
 
-	// We add first runs based on versions
-	return new Configstore('first-run_' + name, {[opts.version]: {firstRun: true}});
+	return new Configstore('first-run_' + name, {firstRun: {version, ran: false}});
 }
 
 function firstRun(opts) {
-	opts = _populateOpts(opts);
-	var version = opts.version.replace(/\./g, '\\.'); // we need to replace the dots with '\\.' for dotProp
 	var firstRun;
-	var conf = getConf(opts);
+	var pkg = readPkgUp.sync({cwd: path.dirname(module.parent.filename)}).pkg;
+	var conf = getConf(opts, pkg);
+	var version = pkg.version;
 
-	if (firstRun === undefined) {
-		firstRun = conf.get(version + ".firstRun");
-	}
+	// perVersion mode
+	if (opts && opts.perVersion) {
+		var info = conf.get('firstRun');
+		var storedVersion = info.version;
+		var firstRun = !info.ran;
 
-	if (firstRun === true || !opts.version) {
-		// only change the value in the config
-		// IF we are actually running the program and not checking if a previous version has been run
-		conf.set(version + '.firstRun', false);
+		if (firstRun) {
+			conf.set('firstRun', {version, ran: true});
+		}
+	} else {
+		// normal mode
+		if (firstRun === undefined) {
+			firstRun = !conf.get('firstRun.ran');
+		}
+
+		if (firstRun === true) {
+			conf.set('firstRun.ran', true);
+		}
 	}
 
 	return firstRun;
 }
 
 function clear(opts) {
-	opts = _populateOpts(opts);
-	var version = opts.version.replace(/\./g, '\\.');
-	getConf(opts).set(version + ".firstRun", true);
-}
-
-function _populateOpts(opts) {
-	opts = opts || {};
-	opts.version = opts.version || readPkgUp.sync({cwd: path.dirname(module.parent.filename)}).pkg.version;
-	return opts;
+	getConf(opts).set('firstRun.ran', false);
 }
 
 module.exports = firstRun;
